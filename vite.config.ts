e794549ca1f30eabd904b943d710e6cb3fa1fcaa
May <abeillee@bk.ru>
2025-08-23 21:30:19 +0300

    chore: update config for tests

--- vite.config.ts
+++ vite.config.ts
@@ -22,4 +22,35 @@ export default defineConfig({
       '@': path.resolve(__dirname, './src'),
     },
   },
+  test: {
+    environment: 'jsdom',
+    setupFiles: ['./src/tests/setupTests.ts'],
+    globals: true,
+    testTimeout: 10000,
+    coverage: {
+      reporter: ['text', 'lcov', 'html'],
+      include: ['src/**/*.{ts,tsx}'],
+      exclude: [
+        'src/**/*.test.{js,jsx,ts,tsx}',
+        'src/app/tests/**',
+        'src/**/index.*',
+        'src/**/constants.*',
+        'src/**/types.*',
+        'src/**/*.d.ts',
+        'src/app/main.tsx',
+        'src/app/app.tsx',
+        'src/**/utils',
+        'src/**/lib',
+        'config/**/*',
+      ],
+      thresholds: {
+        global: {
+          statements: 80,
+          branches: 50,
+          functions: 50,
+          lines: 50,
+        },
+      },
+    },
+  },
 });

6a1c1167d72ace2adfe226dd004b3713ff312010
May <abeillee@bk.ru>
2025-08-24 00:55:54 +0300

    test: add tests for not found page

---
+++ src/pages/not-found-page/not-found-page.test.tsx
@@ -0,0 +1,43 @@
+import { render, screen } from '@testing-library/react';
+import { NotFoundPage } from './not-found-page';
+import { MemoryRouter } from 'react-router-dom';
+import userEvent from '@testing-library/user-event';
+
+const navigateMock = vi.fn();
+
+describe('Not found page tests', () => {
+  beforeAll(() => {
+    vi.mock('react-router-dom', async () => {
+      return {
+        ...(await vi.importActual('react-router-dom')),
+        useNavigate: () => navigateMock,
+      };
+    });
+  });
+
+  test('should render correctly', () => {
+    render(
+      <MemoryRouter>
+        <NotFoundPage />
+      </MemoryRouter>
+    );
+
+    expect(screen.getByText('Oops...Page not found')).toBeInTheDocument();
+    expect(screen.getByAltText('notFound')).toBeInTheDocument();
+    expect(screen.getByRole('button', { name: 'Back' })).toBeInTheDocument();
+  });
+
+  test('should navigate when button clicked', async () => {
+    const user = userEvent.setup();
+    render(
+      <MemoryRouter>
+        <NotFoundPage />
+      </MemoryRouter>
+    );
+
+    const button = screen.getByRole('button', { name: 'Back' });
+
+    await user.click(button);
+    expect(navigateMock).toHaveBeenCalledOnce();
+  });
+});
